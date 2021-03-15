bb5e0f1fa327a2f5fd73d140470674d8b4d06b1f
raamonuribe <79480084+raamonuribe@users.noreply.github.com>
2021-03-14 18:35:09 -0700

    Update createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/raamonuribe/jenkins-aws-example.git'
+                    }
+                    branch 'main'
+                }
+            }
+        }
+    }
+}
